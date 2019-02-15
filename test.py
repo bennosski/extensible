735c9f076ff67b68ab0f6c8d2a7ebbb43e10f41e
Ben <bln@stanford.edu>
2019-02-14 16:13:42 -0800

    added examples

--- test.py
+++ test.py
@@ -1,14 +1,25 @@
 from extensible import extensible
 
-@extensible('x', 'y')
+@extensible()
 def f(x, y):
     return x * y
 
-print(f(2,3))
+print(f(2,3))           # evaluate the function as normal
 
-print(f([1,2,3], 2))
+print(f([1,2,3], 2))    # evaluate the function for each value in first arg, holding second arg constant
 
-print(f([1,2,3], [1,2,3]))
+print(f([1,2,3], [1,2,3])) # evaluate the function while iterating over both args
+
+
+@extensible('x')
+def g(x, y):
+    return x*sum(y)
+
+print(g([1,2,3], [1,2,3]))  # evaluate the function while iterating over 'x' (do not iterate over 'y')
+
+
+
+
 
 
 
